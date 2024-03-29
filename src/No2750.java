e92c92740d0df3a9f77ec4d049dfee6b1135cd51
KMS06 <jjm336949@dsm.hs.kr>
2024-03-29 11:57:15 +0900

    2750

---
+++ src/No2750.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class No2750 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = scan.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
