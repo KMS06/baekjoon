14fdd82f683e366e88e49e9107e50928b31e8a7d
KMS06 <jjm336949@dsm.hs.kr>
2024-04-02 16:07:06 +0900

    2743

---
+++ src/No2743.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class No2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+
+        bw.write(String.valueOf(s.length()));
+        bw.close();
+    }
+}
