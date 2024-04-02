28ebbcfe5d111f22f1b1f6f9fc355013fd054802
KMS06 <jjm336949@dsm.hs.kr>
2024-04-02 09:14:54 +0900

    15894

---
+++ src/No15894.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class No15894 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextInt();
+        System.out.println(n*4);
+    }
+}
